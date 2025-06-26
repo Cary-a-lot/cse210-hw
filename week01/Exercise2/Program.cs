df0bb1ec00edb5c0f99a809c39a7bea28ccf6cbc
Cary-a-lot <cho23011@byui.edu>
2025-06-26 15:05:44 -0600

    excersize 2 first bit, ask for input, and assign input as an interger

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What Grade did you get?");
+        Console.Write("Please input a number without the use of symbols" );
+        string number = Console.ReadLine();
+        int percent = int.Parse(number);
+
     }
 }
