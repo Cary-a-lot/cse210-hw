08d45754572236c3c3e69df947edf140d2900363
Cary-a-lot <cho23011@byui.edu>
2025-06-26 14:44:30 -0600

    finished version of excercise one

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,10 @@ class Program
     {
         Console.Write("What is your first name? ");
         string firstname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+// the money symbol ->$<- is important or else the sting imputs won't connect.
+        Console.WriteLine($"Your name is {lastname},{firstname} {lastname}");
     }
 }
