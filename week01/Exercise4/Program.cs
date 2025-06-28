a3e9710f1302e809196c49d76e0a0cf6c4295254
Cary-a-lot <cho23011@byui.edu>
2025-06-28 16:49:10 -0600

    step one done on excercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        //Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        // numberlist step 1
+        List<int> numbers = new List<int>();
+
+        int numberinput;
+        do
+        {
+            Console.Write("Enter a number (0 to stop): ");
+            numberinput = int.Parse(Console.ReadLine());
+
+            if (numberinput != 0)
+            {
+                numbers.Add(numberinput);
+            }
+        } while (numberinput != 0);
     }
 }
