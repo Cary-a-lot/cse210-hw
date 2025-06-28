68247a681805624dbb34a51d3351e946d0070e34
Cary-a-lot <cho23011@byui.edu>
2025-06-28 17:32:38 -0600

    step 3 and 4 done

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -27,5 +27,20 @@ class Program
             sum += number;
         }
         Console.WriteLine($"The sum is: {sum}");
+
+        //step 3time
+        float average = (float)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        //obligatory step 4
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The maximum is: {max}");
     }
 }
