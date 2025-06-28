d9e5dc1bc5c3b19326f0349e722585521071fe50
Cary-a-lot <cho23011@byui.edu>
2025-06-28 12:56:17 -0600

    grade sorting complete

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -8,13 +8,30 @@ class Program
         Console.Write("Please input a number without the use of symbols" );
         string number = Console.ReadLine();
         int percent = int.Parse(number);
-
+// Grade sorting below
         string Grade = "";
 
         if (percent >= 90)
         {
             Grade = "A";
         }
+        else if (percent >= 80)
+        {
+            Grade = "B";
+        }
+        else if (percent >= 70)
+        {
+            Grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            Grade = "D";
+        }
+        else if (percent <= 59)
+        {
+            Grade = "F";
+        }
+//end of grade sording
 
     }
 }
