d4c39ea9a8fb20a216074fcd34004a66f62a1e17
Cary-a-lot <cho23011@byui.edu>
2025-06-28 16:10:20 -0600

    steps 1, 2, and 3 completed.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,34 @@
 using System;
 
+using System;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+//generating for step 3 random magic number
+        Random randomnumber = new Random();
+        int magicnumber = randomnumber.Next(1, 101); // 1 to 100
+
+        int guess = -1; // I kept wanting to do +3 but i dont think thatll work unless i make the range diffrent.
+
+        while (guess != magicnumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine()); 
+
+            if (guess < magicnumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicnumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed the magic number!");
+            }
+        }
     }
 }
