c5b4d3bd86804085a9d52ae2eb60264ba184e835
Cary-a-lot <cho23011@byui.edu>
2025-06-28 18:00:57 -0600

    function 2 done

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
+    // fun 1 DisplayWelcome
     static void DisplayWelcome()
     {
         Console.WriteLine("Welcome to the program!");
     }
+    //fun 2 prompt username
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    
 }
