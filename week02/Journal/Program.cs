d74a56630511138829f7b7b803d6faff5da4cef0
Cary-a-lot <cho23011@byui.edu>
2025-07-05 20:48:20 -0600

    menu page done,

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         //Console.WriteLine("Hello World! This is the Journal Project.");
+        
+        Journal journal = new Journal();
+        bool isRunning = true;
+
+        while (isRunning)
+        {
+            Console.WriteLine("\nJournal Menu:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("5. Exit");
+            Console.Write("Choose an option: ");
+            string option = Console.ReadLine();
     }
 }
