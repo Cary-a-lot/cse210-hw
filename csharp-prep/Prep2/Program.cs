48c051dd9324a44ebc73daa355010c9743808b5a
Cary-a-lot <cho23011@byui.edu>
2026-01-10 16:43:09 -0700

    prep 2 part of step one complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Ask the user for their grade percentage, then write a series of if-elif-else 
+        // statements to print out the appropriate letter grade. (At this point, you'll
+        // have a separate print statement for each grade letter in the appropriate block.)
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int x = int.Parse(grade);
+
+        Console.WriteLine(grade);
+
     }
 }
