7f53b46fe3709959a376ebca1fbe966de0b88efe
Cary-a-lot <cho23011@byui.edu>
2026-01-15 19:34:00 -0700

    user guided Random number complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,14 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    //Start by asking the user for the magic number. 
+    // (In future steps, we will change this to have the computer generate 
+    // a random number, but to get started, 
+    // we'll just let the user decide what it is.)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string magicNumber = Console.ReadLine();
+
+        Console.WriteLine(magicNumber);
     }
 }
