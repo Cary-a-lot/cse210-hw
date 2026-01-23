731cb97e19e93eb31089777012fd465fb31cd31c
Cary-a-lot <cho23011@byui.edu>
2026-01-22 19:12:24 -0700

    job class created

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Example usage
+        Job job = new Job();
+        job._jobTitle = "Developer";
+        job._company = "ACME";
+        job._startYear = 2020;
+        job._endYear = 2023;
+        job.Display();
+    }
+
+    public class Job
+    {
+        // Member variables
+        public string _jobTitle;
+        public string _company;
+        public int _startYear;
+        public int _endYear;
+
+        // Method to display job details
+        public void Display()
+        {
+            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+        }
     }
 }
