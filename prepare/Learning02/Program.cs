89bfb82c81171588675077ed819de310df2ba267
Cary-a-lot <cho23011@byui.edu>
2026-01-22 19:21:14 -0700

    had to put my classes in the proper spots, i now understand that the job.cs file isnt where we store stuff it is itself a program doing things and communicating with our other program.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,27 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Example usage
         Job job = new Job();
         job._jobTitle = "Developer";
         job._company = "ACME";
         job._startYear = 2020;
         job._endYear = 2023;
+
         job.Display();
-    }
-
-    public class Job
-    {
-        // Member variables
-        public string _jobTitle;
-        public string _company;
-        public int _startYear;
-        public int _endYear;
-
-        // Method to display job details
-        public void Display()
-        {
-            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
-        }
     }
 }
