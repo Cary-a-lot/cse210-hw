5f5505d92f90b0a2b2104b32eb005c03bfc8a419
Cary-a-lot <cho23011@byui.edu>
2026-01-22 19:36:48 -0700

    added another job aka my job and filled in resume for fun

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,15 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Job job = new Job();
-        job._jobTitle = "Developer";
+        job._jobTitle = "Developer of Deadly Traps";
         job._company = "ACME";
         job._startYear = 2020;
         job._endYear = 2023;
 
+        Job job2 = new Job();
+        job2._jobTitle = "Upholstery Quality Manager";
+        job2._company = "RedCo";
+        job2._startYear = 2023;
+        job2._endYear = 2026;
+
         Resume myResume = new Resume();
-        myResume._name = "Your Name";
+        myResume._name = "Cary Choate";
 
         myResume._jobs.Add(job);
+         myResume._jobs.Add(job2);
 
         job.Display();
     }
