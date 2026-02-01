12300a07cb9c341a0b5afc35ad8b3dd1acee8510
Cary-a-lot <cho23011@byui.edu>
2026-01-31 17:39:53 -0700

    start of jounal and the incorperation of entry into journal.

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 class Journal
 {
-    static void Main(string[] args)
+    private List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry entry)
     {
-        
+        _entries.Add(entry);
     }
 }
