0ce16785f1dfebdbb1c9c1e73a9df63d153eb975
Cary-a-lot <cho23011@byui.edu>
2026-01-31 17:15:14 -0700

    prompts made

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class PromptGenerator
 {
-    static void Main(string[] args)
+    private List<string> _prompts = new List<string>
     {
-        
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?",
+        "What was the most random thought I had today?",
+        "Did any past advice help you in your day today?",
+        "What color of socks did you wear today?",
+        "What steps did you take to get closer to your goals?",
+        "How do you feel you have been doing mentally recently?",
+        "What creative ideas have you had today?",
+        "Has any of your favorites changed?"
     }
 }
