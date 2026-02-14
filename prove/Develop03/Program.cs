7edc0211ca83d2543fdead34dc0487e7dcb70fce
Cary-a-lot <cho23011@byui.edu>
2026-02-14 12:17:27 -0700

    scripture chosen and classes set up

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,14 @@
 using System;
 
+using System;
+using System.Collections.Generic;
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference reference = new Reference("Mosiah", 4, 9);
+        Scripture scripture = new Scripture(reference,
+            "Believe in God; believe that he is, and that he created all things, both in heaven and in earth; believe that he has all wisdom, and all power, both in heaven and in earth; believe that man doth not comprehend all the things which the Lord can comprehend.");
     }
 }
