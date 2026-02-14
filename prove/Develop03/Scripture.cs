a74e3a155efa3b9f743d57eb16b2b3143fe69188
Cary-a-lot <cho23011@byui.edu>
2026-02-14 13:12:00 -0700

    Scripture class is completed and plays well with program class everything works as planned.

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -3,4 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class Scripture
-{}
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _random = new Random();
+        _words = text.Split(' ').Select(w => new Word(w)).ToList();
+    }
+
+    public void HideRandomWords(int count)
+    {
+        List<Word> visibleWords = _words.Where(w => !w.IsHidden()).ToList();
+
+        for (int i = 0; i < count && visibleWords.Count > 0; i++)
+        {
+            int index = _random.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        return $"{_reference.GetDisplayText()} {string.Join(" ", _words.Select(w => w.GetDisplayText()))}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(w => w.IsHidden());
+    }
+}
