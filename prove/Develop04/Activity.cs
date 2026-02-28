4f59753eee9a530674bd0409ea3886f38e7caa70
Cary-a-lot <cho23011@byui.edu>
2026-02-28 14:33:26 -0700

    activity class complete

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -20,3 +20,56 @@ class Activity
         PerformActivity();
         EndActivity();
     }
+
+    protected virtual void PerformActivity()
+    {
+    }
+
+    protected void StartActivity()
+    {
+        Console.Clear();
+        Console.WriteLine($"Welcome to the {_name} Activity\n");
+        Console.WriteLine(_description);
+
+        Console.Write("\nHow long, in seconds, would you like for your session? ");
+        _duration = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nPrepare to begin...");
+        ShowSpinner(3);
+    }
+
+    protected void EndActivity()
+    {
+        Console.WriteLine("\nWell done!!");
+        ShowSpinner(3);
+
+        Console.WriteLine($"\nYou have completed the {_name} Activity for {_duration} seconds.");
+        ShowSpinner(3);
+    }
+
+    protected void ShowSpinner(int seconds)
+    {
+        List<string> spinner = new List<string> { "|", "/", "-", "\\" };
+
+        DateTime endTime = DateTime.Now.AddSeconds(seconds);
+        int i = 0;
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write(spinner[i]);
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+            i = (i + 1) % spinner.Count;
+        }
+    }
+
+    protected void ShowCountdown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+    }
+}
