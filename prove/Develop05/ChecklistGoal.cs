9cea872535256fe89e7c2bd3d762242fe69219e2
Cary-a-lot <cho23011@byui.edu>
2026-03-14 13:20:13 -0600

    Eternalgoal and checklist goal complete

---
+++ prove/Develop05/ChecklistGoal.cs
@@ -0,0 +1,38 @@
+using System;
+
+public class ChecklistGoal : Goal
+{
+    private int _targetCount;
+    private int _currentCount;
+    private int _bonus;
+
+    public ChecklistGoal(string name, string description, int points, int targetCount, int bonus)
+        : base(name, description, points)
+    {
+        _targetCount = targetCount;
+        _bonus = bonus;
+        _currentCount = 0;
+    }
+
+    public override int RecordEvent()
+    {
+        _currentCount++;
+
+        if (_currentCount == _targetCount)
+        {
+            return _points + _bonus;
+        }
+
+        return _points;
+    }
+
+    public override bool IsComplete()
+    {
+        return _currentCount >= _targetCount;
+    }
+
+    public override string GetStatusString()
+    {
+        return $"[ ] {_name} -- Completed {_currentCount}/{_targetCount}";
+    }
+}
