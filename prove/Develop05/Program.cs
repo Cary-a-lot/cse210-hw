bacd98c2251d4707afa1f626d8f9e7bd00792b68
Cary-a-lot <cho23011@byui.edu>
2026-03-14 16:41:46 -0600

    program complete

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -2,8 +2,46 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop05 World!");
+        EternalQuestManager manager = new EternalQuestManager();
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("\nEternal Quest Program");
+            Console.WriteLine($"Score: {manager.GetScore()}");
+            Console.WriteLine("1. Create Goal");
+            Console.WriteLine("2. List Goals");
+            Console.WriteLine("3. Record Event");
+            Console.WriteLine("4. Save Goals");
+            Console.WriteLine("5. Load Goals");
+            Console.WriteLine("6. Quit");
+
+            Console.Write("Select option: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    manager.CreateGoal();
+                    break;
+                case "2":
+                    manager.DisplayGoals();
+                    break;
+                case "3":
+                    manager.RecordEvent();
+                    break;
+                case "4":
+                    manager.SaveGoals("goals.txt");
+                    break;
+                case "5":
+                    manager.LoadGoals("goals.txt");
+                    break;
+                case "6":
+                    running = false;
+                    break;
+            }
+        }
     }
 }
